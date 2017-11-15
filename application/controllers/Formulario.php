6fd58eb1aa469ffe320b3f634c1d60c8c5b33433
UrielKaralden <urielkaralden@gmail.com>
2017-11-15 21:31:33 +0100

    13.5 Insertar artículos en la base de datos desde el formulario

--- application/controllers/Formulario.php
+++ application/controllers/Formulario.php
@@ -15,25 +15,29 @@
 
         public function validar()
         {
-            $this->form_validation->set_rules('titulo', 'Título del artículo', 'required|max_length[100]');
-            $this->form_validation->set_rules('descripcion', 'Descripción del artículo', 'required|max_length[200]');
-            $this->form_validation->set_rules('cuerpo', 'Cuerpo del artículo', 'required');
-
-            $this->form_validation->set_message('required', 'El campo %s es obligatorio.');
-            $this->form_validation->set_message('max_length', 'El campo %s tiene como máximo %s caracteres.');
-
-            if($this->form_validation->run()==FALSE)
+            if($this->input->post('submit'))
             {
-                $this->index();
-            }
-            else
-            {
-                $data = array('titulo'=> 'Artículo',
-                    'titulo_articulo'=> $this->input->post('titulo'),
-                    'descripcion'=> $this->input->post('descripcion'),
-                    'cuerpo'=> $this->input->post('cuerpo'));
+                $this->form_validation->set_rules('titulo', 'Título del artículo', 'required|max_length[100]');
+                $this->form_validation->set_rules('descripcion', 'Descripción del artículo', 'required|max_length[200]');
+                $this->form_validation->set_rules('cuerpo', 'Cuerpo del artículo', 'required');
 
-                $this->load->view('un_articulo_view', $data);
+                $this->form_validation->set_message('required', 'El campo %s es obligatorio.');
+                $this->form_validation->set_message('max_length', 'El campo %s tiene como máximo %s caracteres.');
+
+                if($this->form_validation->run()==FALSE)
+                {
+                    $this->index();
+                }
+                else
+                {
+                    $data = array('titulo'=> 'Artículo',
+                        'titulo_articulo'=> $this->input->post('titulo'),
+                        'descripcion'=> $this->input->post('descripcion'),
+                        'cuerpo'=> $this->input->post('cuerpo'));
+
+                    $this->Articulos_model->add_articulo();
+                    $this->load->view('un_articulo_view', $data);
+                }
             }
         }
     }
