e1625c0d637fe92ea8362ea8631227adb8502fef
LuisIM21 <RiBan2012@gmail.com>
2018-01-08 21:01:35 +0100

    video models

--- application/models/videos_model.php
+++ application/models/videos_model.php
@@ -3,21 +3,10 @@
 		public function _construct(){
 			parent::_construct();
 		}
-
-		public function get_articulos(){
+		
+		public function get_videos(){
 			$consulta = $this->db->query('Select * from videos;');
 			return $consulta->result();
-		}
-
-		function get_search()
-		{
-			$match = $this->input->post('search');
-			$this->db->like('url_video',$match);
-			$this->db->or_like('usuario', $match);
-			$this->db->or_like('titulo', $match);
-			$this->db->or_like('Fecha', $match);
-			$query = $this->db->get('videos');
-			return $query->result();
 		}
 	}
 ?>
